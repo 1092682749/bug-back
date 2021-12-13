f02bee1b28136a464259dc6f815a6fc5acef9d68
墨墨 <36019799+1092682749@users.noreply.github.com>
2021-12-13 23:08:20 +0800

    readme

--- log4j-bug/src/main/java/com/dyz/log4j/bug/back/hacker/logic/Attack.java
+++ log4j-bug/src/main/java/com/dyz/log4j/bug/back/hacker/logic/Attack.java
@@ -8,6 +8,5 @@ public class Attack extends UnicastRemoteObject {
     public Attack() throws RemoteException {
         super();
         System.out.println("当你在服务端看到这句话，就意味着我可以在这里放任何攻击你的代码！");
-
     }
 }
