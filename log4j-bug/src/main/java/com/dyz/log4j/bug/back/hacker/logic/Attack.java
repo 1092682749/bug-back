7051a85f6b169bbe03f07c0bd2f5495dd079ac7d
墨墨 <36019799+1092682749@users.noreply.github.com>
2021-12-13 23:30:40 +0800

    加注释

--- log4j-bug/src/main/java/com/dyz/log4j/bug/back/hacker/logic/Attack.java
+++ log4j-bug/src/main/java/com/dyz/log4j/bug/back/hacker/logic/Attack.java
@@ -7,6 +7,7 @@ public class Attack extends UnicastRemoteObject {
 
     public Attack() throws RemoteException {
         super();
+        // 添加任何攻击逻辑，比如使用runtime执行操作系统命令。或者窃取信息到攻击者的服务器。
         System.out.println("当你在服务端看到这句话，就意味着我可以在这里放任何攻击你的代码！");
     }
 }
